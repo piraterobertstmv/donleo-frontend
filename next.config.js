1930cba3a6a5c865c353160340dece47189e9eb7
Antonio Moya <amoyavalls@gmail.com>
2026-01-30 19:48:12 +0000

    Fix(config): enable dynamicIO for next-intl prerendering

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    dynamicIO: true,
+  },
+}
 
 module.exports = withNextIntl(nextConfig);
