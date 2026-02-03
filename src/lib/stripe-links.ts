553c565a41997ce9072481ef13f09c03dbec6bf0
Antonio Moya <amoyavalls@gmail.com>
2026-02-03 20:00:06 +0000

    chore: use live Stripe payment links

--- src/lib/stripe-links.ts
+++ src/lib/stripe-links.ts
@@ -3,7 +3,7 @@
  * Used everywhere subscription/upgrade is offered (all languages, all pages)
  */
 export const STRIPE_PAYMENT_LINKS = {
-  weekly: 'https://buy.stripe.com/test_8x200k2FT16s2VmdLl5J600',
-  monthly: 'https://buy.stripe.com/test_cNi7sM5S5cPa1RidLl5J602',
-  annual: 'https://buy.stripe.com/test_dRm3cw3JXaH27bCcHh5J601',
+  weekly: 'https://buy.stripe.com/cNifZi6XI6tR2AmdURejK02',
+  monthly: 'https://buy.stripe.com/cNi6oI81McSf5MybMJejK01',
+  annual: 'https://buy.stripe.com/eVq14o1Do2dBa2O6spejK00',
 } as const
